7d767a9674ebba05505f1ddb1b6668da9cf40044
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2015-05-30 16:28:32 +0200

    Add subsites

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,4 +7,24 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.Index())
   }
+
+  def aboutAllgemeines = Action {
+    Ok(views.html.about.Allgemeines())
+  }
+
+  def aboutFachschaften = Action {
+    Ok(views.html.about.Fachschaften())
+  }
+
+  def aboutGeschichte = Action {
+    Ok(views.html.about.Geschichte())
+  }
+
+  def stapf = Action {
+    Ok(views.html.Stapf())
+  }
+
+  def beschluesse = Action {
+    Ok(views.html.Beschluesse())
+  }
 }
