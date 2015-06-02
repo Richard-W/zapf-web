3943d90fc394a720a2c27cd5af1988ebe9b8eb7e
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2015-06-03 00:08:13 +0200

    Add password changing

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,8 @@ package controllers
 import play.api.mvc._
 import models._
 import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.Future
+import scala.util.Left
 
 object Application extends Controller {
 
@@ -45,13 +47,31 @@ object Application extends Controller {
   }
 
   def profilePost = Action.async { implicit request ⇒
-    RequestContext() map { implicit requestContext ⇒
+    RequestContext() flatMap { implicit requestContext ⇒
       if(!requestContext.user.isDefined) {
-        Redirect(routes.Application.index).flashing("error" -> "Du musst dich einloggen, bevor du auf dein Profil zugreifen kannst.")
+        Future.successful(
+          Redirect(routes.Application.index).flashing("error" -> "Du musst dich einloggen, bevor du auf dein Profil zugreifen kannst.")
+        )
       } else {
         val map = request.body.asFormUrlEncoded.get
-        requestContext.user.get.copy(firstName = map("firstName")(0), lastName = map("lastName")(0), email = map("email")(0)).update
-        Redirect(routes.Application.profile)
+        map("section")(0) match {
+          case "account" ⇒
+            requestContext.user.get.copy(
+              firstName = map("firstName")(0),
+              lastName = map("lastName")(0),
+              email = map("email")(0)
+            ).update map { _ ⇒
+              Redirect(routes.Application.profile).flashing("success" -> "Accountinformationen aktualisiert.")
+            }
+          case "cpw" ⇒
+            if(map("pw1")(0) == map("pw2")(0)) {
+              requestContext.user.get.copy(pass = Left(PasswordHash.create(map("pw1")(0)))).update map { _ ⇒
+                Redirect(routes.Application.profile).flashing("success" -> "Passwort aktualisiert.")
+              }
+            } else {
+              Future.successful(Redirect(routes.Application.profile).flashing("error" -> "Passwörter stimmen nicht überein."))
+            }
+        }
       }
     }
   }
